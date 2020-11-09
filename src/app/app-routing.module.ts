4efd970d6d9b0f994e5f37d98e51c49443058a7f
Louise Alberti <albertilouise@gmail.com>
2020-11-09 06:53:16 -0300

    mudancas nos css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './../home/home.component';
-import { PragasComponent } from '../pragas/pragas.component';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { PragasComponent } from './pragas/pragas.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  { path: 'pragas', component: PragasComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'pragas', component: PragasComponent },
   { path: '**', component: PageNotFoundComponent },//esse path deve ser sempre o último
-]
+];
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
